146c8e1254b497bb9e42e7524bd1c0284acc75dd
kartsridhar <contactkarthik34@gmail.com>
2019-06-24 18:35:46 +0100

    added input

--- recommender.py
+++ recommender.py
@@ -13,24 +13,33 @@ df = pd.merge(df_ratings, df_movies, on="movieId")
 # All this done by pivot_table
 rate_mat = df.pivot_table(index="userId", columns="title", values="rating")
 
-# Extracting the ratings for a particular movie
-forrestGumpRatings = rate_mat['Fight Club (1999)']
+movie_name = input("Enter a movie name: ")
+
+# Extracting the ratings for the respective movie
+movieRatings = rate_mat[movie_name]
 
 # Finding similar movies ==> Correlations using corrwith()
 # compute the pairwise correlation of movie vector of rate_mat
 # with every other movie
-similar = rate_mat.corrwith(forrestGumpRatings)
+similarMovies = rate_mat.corrwith(movieRatings)
 
 # Construct a new DataFrame of movies and their correlation score
-corrForrest = pd.DataFrame(similar, columns=['Correlation'])
+df_corr = pd.DataFrame(similarMovies)
 
 # Remove all the NaN values
-corrForrest.dropna(inplace=True)
-movieStats = df.groupby('title').agg({'rating': [np.size, np.mean]})
+df_corr.dropna(inplace=True)
 
-popularMovies = movieStats['rating']['size'] >= 100
-movieStats[popularMovies].sort_values([('rating', 'mean')], ascending=False)[:15]
+# Creating a DataFrame counting the number of ratings and overall mean
+df_stats = df.groupby('title').agg({'rating': [np.size, np.mean]})
 
-df_new = movieStats[popularMovies].join(pd.DataFrame(similar, columns=['similarity']))
-df_new = df_new.sort_values(['similarity'], ascending=False)[:15]
-print(df_new.head())
+# Choosing movies only with over 200 ratings
+popularMovies = df_stats['rating']['size'] >= 200
+
+# Sorting the mean ratings in descending order, choosing the first 10 movies
+df_stats[popularMovies].sort_values([('rating', 'mean')], ascending=False)[:10]
+
+# Joining the above data to the similarMovies set
+df_result = df_stats[popularMovies].join(pd.DataFrame(similarMovies, columns=['Similarity']))
+
+df_result = df_result.sort_values(['Similarity'], ascending=False)[:10]
+print(df_result.head())
