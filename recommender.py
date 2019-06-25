4ef73b1d11b5cfbdfdbd8cbf1262ea5b562a5e3f
kartsridhar <contactkarthik34@gmail.com>
2019-06-26 00:20:07 +0100

    working on recommendations now

--- recommender.py
+++ recommender.py
@@ -6,40 +6,28 @@ df_movies = pd.read_csv('data/movies.csv')
 df_ratings = pd.read_csv('data/ratings.csv')
 
 # Merging the frames over movieID
-df = pd.merge(df_ratings, df_movies, on="movieId")
+df = pd.merge(df_ratings, df_movies)
 
 # Generating a user/movie rating matrix.
 # NaN = user did not rate that particular movie
 # All this done by pivot_table
 rate_mat = df.pivot_table(index="userId", columns="title", values="rating")
 
-movie_name = input("Enter a movie of the form MOVIE_NAME (YEAR_OF_RELEASE): ")
+# find the correlation matrix. min period means at least 150
+corrMatrix = rate_mat.corr(min_periods=150)
 
-# Extracting the ratings for the respective movie
-movieRatings = rate_mat[movie_name]
+user_0_rat = rate_mat.iloc[0].dropna()
 
-# Finding similar movies ==> Correlations using corrwith()
-# compute the pairwise correlation of movie vector of rate_mat
-# with every other movie
-similarMovies = rate_mat.corrwith(movieRatings)
+# creating a list similar to the ones I rated
+simCandidates = pd.Series()
+for i in range(0, len(user_0_rat.index)):
+    print("Adding sims for " + user_0_rat.index[i] + "...")
 
-# Construct a new DataFrame of movies and their correlation score
-df_corr = pd.DataFrame(similarMovies)
+    #Retrieve similar movies that I rated
+    sims = corrMatrix[user_0_rat.index[i]].dropna()
+    sims = sims.map(lambda x : x * user_0_rat[i])
+    simCandidates = simCandidates.append(sims)
 
-# Remove all the NaN values
-df_corr.dropna(inplace=True)
-
-# Creating a DataFrame counting the number of ratings and overall mean
-df_stats = df.groupby('title').agg({'rating': [np.size, np.mean]})
-
-# Choosing movies only with over 200 ratings
-popularMovies = df_stats['rating']['size'] >= 200
-
-# Sorting the mean ratings in descending order, choosing the first 10 movies
-df_stats[popularMovies].sort_values([('rating', 'mean')], ascending=False)[:10]
-
-# Joining the above data to the similarMovies set
-df_result = df_stats[popularMovies].join(pd.DataFrame(similarMovies, columns=['Similarity']))
-
-df_result = df_result.sort_values(['Similarity'], ascending=False)[:10]
-print(df_result.head())
+print("Sorting...")
+simCandidates.sort_values(inplace=True, ascending=False)
+print(simCandidates.head())
---
+++ similar.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+# Get the movies and ratings data frame
+df_movies = pd.read_csv('data/movies.csv')
+df_ratings = pd.read_csv('data/ratings.csv')
+
+# Merging the frames over movieID
+df = pd.merge(df_ratings, df_movies, on="movieId")
+
+# Generating a user/movie rating matrix.
+# NaN = user did not rate that particular movie
+# All this done by pivot_table
+rate_mat = df.pivot_table(index="userId", columns="title", values="rating")
+
+movie_name = input("Enter a movie of the form MOVIE_NAME (YEAR_OF_RELEASE): ")
+
+# Extracting the ratings for the respective movie
+movieRatings = rate_mat[movie_name]
+
+# Finding similar movies ==> Correlations using corrwith()
+# compute the pairwise correlation of movie vector of rate_mat
+# with every other movie
+similarMovies = rate_mat.corrwith(movieRatings)
+
+# Construct a new DataFrame of movies and their correlation score
+df_corr = pd.DataFrame(similarMovies)
+
+# Remove all the NaN values
+df_corr.dropna(inplace=True)
+
+# Creating a DataFrame counting the number of ratings and overall mean
+df_stats = df.groupby('title').agg({'rating': [np.size, np.mean]})
+
+# Choosing movies only with over 200 ratings
+popularMovies = df_stats['rating']['size'] >= 200
+
+# Sorting the mean ratings in descending order, choosing the first 10 movies
+df_stats[popularMovies].sort_values([('rating', 'mean')], ascending=False)[:10]
+
+# Joining the above data to the similarMovies set
+df_result = df_stats[popularMovies].join(pd.DataFrame(similarMovies, columns=['Similarity']))
+
+df_result = df_result.sort_values(['Similarity'], ascending=False)[:10]
+print(df_result.head())
